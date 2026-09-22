547d198780a3424e86eeaa67d82251da48b89972
Sahana R <sahanar8050@gmail.com>
2026-09-22 18:31:11 +0530

    Fix formatting in arthematicOperations.java
    Arthematic_Operations

--- arthematicOperations.java
+++ arthematicOperations.java
@@ -3,7 +3,7 @@ public class arthematicOperations {
     public static void main(String a[])
     {
        int num1  =10;
-       int num2  =30;
+       int num2  =30; 
          int r=num1+num2;  // ,- ,* ,/ ,%, 
 
          //incrementing 

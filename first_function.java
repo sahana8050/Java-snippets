0f950b1f7be5c87f96d4248a533e42c5d8306fb1
Sahana <sahanar8050@gmail.com>
2026-09-23 22:19:21 +0530

    calling function

---
+++ first_function.java
@@ -0,0 +1,12 @@
+public class first_function {
+    public static void main(String[] args)
+    {
+        nothingwilldo(); //function invocation calling function 
+    }
+    public static void nothingwilldo()// function definition 
+    {
+
+    }
+    
+
+}

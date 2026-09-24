fd1865ae68d6f97cfe581e1e9e3ab2837c3eb3fc
Sahana <sahanar8050@gmail.com>
2026-09-24 22:27:33 +0530

    Basics

---
+++ Student_details.java
@@ -0,0 +1,22 @@
+public class Student_details {
+    public static void main(String[] args)
+    {
+        static void  printStudentDetails(int studentID)
+        {
+          String name = "sahana";
+           int age = 18;
+           int grade=76;
+           double CGPA=8.2;
+           boolean isRegulartoschool=true;
+
+           System.out.println(name);
+           System.out.println(age);
+           System.out.println(grade);
+           System.out.println(CGPA);
+           System.out.println(isRegulartoschool);
+
+           }}
+
+
+    
+}
